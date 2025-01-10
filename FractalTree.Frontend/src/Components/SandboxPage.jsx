30139009e1eb10d202d38e24c8efe02f7af2a16d
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-01-10 11:46:22 +0300

    Frontend: Make sandbox page UI
    Co-Authored-By: Fractal Tree <194416362+TeamFractalTree@users.noreply.github.com>

--- FractalTree.Frontend/src/Components/SandboxPage.jsx
+++ FractalTree.Frontend/src/Components/SandboxPage.jsx
@@ -1,10 +1,29 @@
 import BottomBar from "./BottomBar";
 import Header from "./Header";
+import "../CSS/SandboxPage.css"
+import { IconSandbox } from "@tabler/icons-react";
+import { Button } from 'primereact/button';
+import { IconTextScan2 } from "@tabler/icons-react";
 
 export default function SandboxPage() {
     return (
         <>
             <Header>Sandbox</Header>
+
+            <div className="sandboxPage">
+                <div className="sandboxSplash">
+                    <IconSandbox></IconSandbox>
+                </div>
+                <p className="sandboxDescription">Sandbox mode allows you to quickly scan and run code without saving it.</p>
+
+                <Button className="sandboxContinue">
+                    <IconTextScan2/>
+                    &nbsp;
+                    Go
+                </Button>
+            </div>
+
+
             <BottomBar></BottomBar>
         </>
     )
