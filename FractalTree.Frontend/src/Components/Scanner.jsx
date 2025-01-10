98b87d2b2a2726c7ec7b11f7ca5d4f4b9c591c5e
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-01-10 18:17:51 +0300

    Frontend: Make scanner overlay work on iPhone
    Co-Authored-By: Fractal Tree <194416362+TeamFractalTree@users.noreply.github.com>

--- FractalTree.Frontend/src/Components/Scanner.jsx
+++ FractalTree.Frontend/src/Components/Scanner.jsx
@@ -13,7 +13,7 @@ export default function Scanner() {
     return (
         <Sidebar style={{ height: "100%" }} position="bottom" className="scannerContainer" visible={scannerVisible} onHide={() => setScannerVisible(false)}>
             <Webcam className="camera" audio={false} videoConstraints={{ facingMode: "environment" }} ></Webcam>
-            <img src="/ScannerBackground.png" className="scannerOverlay">
+            <img src="/ScannerOverlayBackground.png" className="scannerOverlay">
 
             </img>
         </Sidebar>
