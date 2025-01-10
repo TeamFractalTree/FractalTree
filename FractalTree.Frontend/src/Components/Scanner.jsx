c56e2d24f2d253c663c14be5b36ece60b51182c6
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-01-10 19:13:50 +0300

    Frontend: Add scan button to scanner
    Co-Authored-By: Fractal Tree <194416362+TeamFractalTree@users.noreply.github.com>

--- FractalTree.Frontend/src/Components/Scanner.jsx
+++ FractalTree.Frontend/src/Components/Scanner.jsx
@@ -4,18 +4,32 @@ import { useState } from 'react';
 import "../CSS/Scanner.css"
 import { useRef } from 'react';
 import Webcam from "react-webcam";
+import { IconTextScan2 } from '@tabler/icons-react';
+import { ProgressSpinner } from 'primereact/progressspinner';
 
 export default function Scanner() {
 
     var [scannerVisible, setScannerVisible] = useState(false);
+    var [scanState, setScanState] = useState("none");
+
     window.openScanner = () => setScannerVisible(true);
+
+    var startScan = () => {
+        setScanState("loading");
+    }
 
     return (
         <Sidebar style={{ height: "100%" }} position="bottom" className="scannerContainer" visible={scannerVisible} onHide={() => setScannerVisible(false)}>
             <Webcam className="camera" audio={false} videoConstraints={{ facingMode: "environment" }} ></Webcam>
-            <img src="/ScannerOverlayBackground.png" className="scannerOverlay">
-
-            </img>
+            <img src="/ScannerOverlayBackground.png" className="scannerOverlay"></img>
+            <Button onClick={startScan} disabled={scanState == "loading"} className="scanButton">
+                {
+                    // Show Loading Wheel If Loading State Is Active
+                    scanState == "loading" ?
+                    <ProgressSpinner strokeWidth="8"/> :
+                    <IconTextScan2/>
+                }
+            </Button>
         </Sidebar>
     )
 }
