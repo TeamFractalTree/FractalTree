926ac5a0de3ab4f586f144fc192f7eafc666de1b
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-01-10 21:02:30 +0300

    Backend: Create asp.net core project
    Co-Authored-By: Fractal Tree <194416362+TeamFractalTree@users.noreply.github.com>

---
+++ FractalTree.Backend/FractalTree.Backend.API/Controllers/ScanningController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FractalTree.Backend.API.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class ScanningController : ControllerBase
+    {
+        [HttpPost("scan")]
+        public async Task<IActionResult> Scan()
+        {
+            return Ok("I'm nothing like y'all 🗣️🗣️🔥🔥🐟🐟");
+        }
+    }
+}
