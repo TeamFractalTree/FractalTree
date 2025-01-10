4a83e483ef4cc2c69c3d4487d9b6e9cb4f505eac
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-01-10 20:39:37 +0300

    Frontend: Make scanner actually scan
    Co-Authored-By: Fractal Tree <194416362+TeamFractalTree@users.noreply.github.com>

--- FractalTree.Frontend/src/Components/Scanner.jsx
+++ FractalTree.Frontend/src/Components/Scanner.jsx
@@ -7,23 +7,57 @@ import Webcam from "react-webcam";
 import { IconTextScan2 } from '@tabler/icons-react';
 import { ProgressSpinner } from 'primereact/progressspinner';
 import Vibrate from '../Helpers/Vibrator';
+import Image from "image-js"
+import { saveAs } from 'file-saver';
 
 export default function Scanner() {
 
     var [scannerVisible, setScannerVisible] = useState(false);
     var [scanState, setScanState] = useState("none");
 
+    var cameraRef = useRef(null);
+    var targetRef = useRef(null);
+
     window.openScanner = () => setScannerVisible(true);
 
     var startScan = () => {
+        if (!cameraRef.current) { return; }
+
         setScanState("loading");
         Vibrate();
+
+        setTimeout(async () => {
+            var screenshot = cameraRef.current.getScreenshot(); // Returns A WebP Data URL
+            var image = await Image.load(screenshot);
+
+            var parentDimensions = cameraRef.current.video.getBoundingClientRect();
+            var targetDimensions = targetRef.current.getBoundingClientRect();
+
+            var scale = image.width / parentDimensions.width;
+            var captureX = Math.abs(parentDimensions.x - targetDimensions.x);
+            var captureY = Math.abs(parentDimensions.y + targetDimensions.y);
+
+            image = image.crop({
+                x: captureX * scale, 
+                y: captureY * scale, 
+                width: targetDimensions.width * scale, 
+                height: targetDimensions.height * scale
+            });
+
+            var savedImage = await image.toBlob("image/png");
+            saveAs(savedImage);
+
+            setScanState("none");
+        });
     }
 
     return (
         <Sidebar style={{ height: "100%" }} position="bottom" className="scannerContainer" visible={scannerVisible} onHide={() => setScannerVisible(false)}>
-            <Webcam className="camera" audio={false} videoConstraints={{ facingMode: "environment" }} ></Webcam>
+
+            <Webcam forceScreenshotSourceSize={true} ref={cameraRef} className="camera" audio={false} videoConstraints={{ facingMode: "environment" }} ></Webcam>
             <img src="/Images/ScannerOverlayBackground.png" className="scannerOverlay"></img>
+            <div ref={targetRef} className="scannerTarget"></div>
+
             <Button onClick={startScan} disabled={scanState == "loading"} className="scanButton">
                 {
                     // Show Loading Wheel If Loading State Is Active
