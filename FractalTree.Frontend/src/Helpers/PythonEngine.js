5b2d16eac7a858d6b1ad3c0eab35d9e921987929
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-01-11 19:48:12 +0300

    Frontend: Add support for interactive prompts
    Co-Authored-By: Fractal Tree <194416362+TeamFractalTree@users.noreply.github.com>

--- FractalTree.Frontend/src/Helpers/PythonEngine.js
+++ FractalTree.Frontend/src/Helpers/PythonEngine.js
@@ -4,7 +4,26 @@ window.pyodide = await loadPyodide({
     indexURL: "/Pyodide",
 });
 
-pyodide.setStdin({ stdin: () => prompt() });
+pyodide.setStdin({ stdin: () => prompt(window.pythonPromptString || "Python code is asking for a value:") });
+pyodide.registerJsModule("fractaltree", { 
+    prePrompt: (promptString) => {
+        window.pythonPromptString = promptString;
+    }
+});
+
+var injectedPythonLib = `
+import builtins
+import time
+from fractaltree import prePrompt
+
+def input(prompt):
+    prePrompt(prompt)
+    time.sleep(0)
+    value = builtins.input(prompt)
+    print("")
+    return value
+
+`;
 
 export default async function ExecutePython(code, stdOut) {
 
@@ -14,7 +33,7 @@ export default async function ExecutePython(code, stdOut) {
     pyodide.setStdout({ batched: (d) => stdOut(d + "\r\n") });
 
     try {
-        await pyodide.runPythonAsync(code);
+        await pyodide.runPythonAsync(injectedPythonLib + code);
     }
     catch (ex) {
         stdOut("\x1b[38;5;196m"); // Set To Red
