86e61d7344cab694dd7ec7c831fdde402efa90a1
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-01-13 17:04:13 +0300

    Mobile: Make titlebar white
    Co-Authored-By: Fractal Tree <194416362+TeamFractalTree@users.noreply.github.com>

--- FractalTree.MobileApp/src-tauri/gen/android/app/src/main/java/org/fractal_tree/mobile/MainActivity.kt
+++ FractalTree.MobileApp/src-tauri/gen/android/app/src/main/java/org/fractal_tree/mobile/MainActivity.kt
@@ -1,3 +1,13 @@
 package org.fractal_tree.mobile
 
-class MainActivity : TauriActivity()
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class MainActivity : TauriActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.statusBarColor = android.graphics.Color.WHITE
+    }
+}
