4b667109eb582a9c946c33b37f30799cd846b862
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-01-14 09:30:28 +0300

    Frontend: JavaScript engine fixes
    Co-Authored-By: Fractal Tree <194416362+TeamFractalTree@users.noreply.github.com>

--- FractalTree.Frontend/src/Helpers/JavaScriptEngine.js
+++ FractalTree.Frontend/src/Helpers/JavaScriptEngine.js
@@ -2,8 +2,12 @@ export default function ExecuteJavaScript(code, stdOut) {
     stdOut("\x1b[2J\x1b[H"); // Clear Terminal
     stdOut("\x1b[38;5;232m"); // Set To Black
 
-    window.jsStubOut = stdOut;
-    code = code.replaceAll("console.log", "window.jsStubOut")
+    window.jsStubOut = (d) => stdOut(d.toString());
+
+    window.input = prompt;
+    window.print = jsStubOut;
+
+    code = code.replaceAll("console.log", "window.jsStubOut"); // Replace calls to console.log with the stdOut
 
     try {
         eval(code);
