ca19f4a67bf44170d9d27d0793be5b4ef669cd5b
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-01-21 13:51:34 +0300

    Android App: Support dark title bar
    Co-Authored-By: Fractal Tree <194416362+TeamFractalTree@users.noreply.github.com>

--- FractalTree.MobileApp/src-tauri/gen/android/app/src/main/java/org/fractal_tree/mobile/MainActivity.kt
+++ FractalTree.MobileApp/src-tauri/gen/android/app/src/main/java/org/fractal_tree/mobile/MainActivity.kt
@@ -1,13 +1,31 @@
 package org.fractal_tree.mobile
 
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : TauriActivity() {
+
+    private fun isDarkMode(): Boolean {
+        return when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+          Configuration.UI_MODE_NIGHT_YES -> true
+          Configuration.UI_MODE_NIGHT_NO -> false
+          Configuration.UI_MODE_NIGHT_UNDEFINED -> false
+          else -> false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.statusBarColor = android.graphics.Color.WHITE
+
+        if (isDarkMode()) {
+            window.statusBarColor = Color.parseColor("#170d16")
+        } else {
+            window.statusBarColor = android.graphics.Color.WHITE
+        }
+        
     }
 }
