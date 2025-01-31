bc539751599ca576978d6659bb773bd578868515
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-01-31 22:58:18 +0300

    [OTK] Bluetooth sharing working
    Co-Authored-By: Fractal Tree <194416362+TeamFractalTree@users.noreply.github.com>

--- FractalTree.MobileApp/src-tauri/src/lib.rs
+++ FractalTree.MobileApp/src-tauri/src/lib.rs
@@ -3,6 +3,8 @@
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_sharesheet::init())
+        .plugin(tauri_plugin_fs::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
